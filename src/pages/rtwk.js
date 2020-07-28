6d5405bfafbea2965f64c7c0758672b2a6792598
wizardOfOngz <ongtiongwoon@gmail.com>
2020-07-28 12:57:53 +0100

    new rtwk

--- src/pages/rtwk.js
+++ src/pages/rtwk.js
@@ -15,13 +15,52 @@ export default function RTWK() {
       <hr />
       <section className="essay-container normal-text-yellow">
         <p>
+          A theory is like a medicine (or government): often useless, sometimes
+          necessary, always self serving, and on occasion lethal. - Nassim Taleb
+        </p>
+        <p>
+          What are you going to do? Sometimes life's a bitch and then you die.
+          Sometimes life's a bitch and you keep on living. - BoJack and Diane
+        </p>
+        <p>
+          But every happy ending has a day after the happy ending, right? And
+          the day after that. - Diane Nguyen
+        </p>
+        <p>
+          Meaning is not something you stumble across, like the answer to a
+          riddle or the prize in a treasure hunt. Meaning is something you build
+          into your life. You build it out of your own past, out of your
+          affections and loyalties, out of the experience of humankind as it is
+          passed on to you, out of your own talent and understanding, out of the
+          things you believe in, out of the things and people you love, out of
+          the values for which you are willing to sacrifice something. The
+          ingredients are there. You are the only one who can put them together
+          into that unique pattern that will be your life. Let it be a life that
+          has dignity and meaning for you. If it does, then the particular
+          balance of success or failure is of less account. - John Gardner
+        </p>
+        <p>
+          I still think we’re our own jailkeepers, but I’ve concluded that our
+          parents and the society at large have a hand in building our prisons.
+          They create roles for us — and self-images — that hold us captive for
+          a long time. The individual intent on self-renewal will have to deal
+          with ghosts of the past — the memory of earlier failures, the remnants
+          of childhood dramas and rebellions, accumulated grievances and
+          resentments that have long outlived their cause. Sometimes people
+          cling to the ghosts with something almost approaching pleasure — but
+          the hampering effect on growth is inescapable. - John Gardner
+        </p>
+        <p>
           Everyone I have met was the same. For some it is money, for some it is
           women, drinking, children, family, even worshipping gods, dreams,
           power...Everyone had to be drunk on something to keep pushing on.
           Everyone was a slave to something. Even him who called himself King. -
           Kenny Ackermann
         </p>
-        <p>If you win, you live. If you lose, you die. If you don't fight, you can't win - Eren Yaeger</p>
+        <p>
+          If you win, you live. If you lose, you die. If you don't fight, you
+          can't win - Eren Yaeger
+        </p>
         <p>
           Chasing good experience all the time is itself a bad experience,
           accepting bad experience is itself a good experience. - Mark Manson
